0a801307f1a5804de9546bbba63d781a413e758a
Tit Petric <tit.petric@monotek.net>
2016-04-20 07:59:00 -0400

    import manet and webshot

---
+++ webshot-cli/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+DEBIAN_FRONTEND=noninteractive
+
+# install dependencies for build
+
+apt-get -qq update
+apt-get -qq -y install build-essential xvfb libfontconfig1 curl
+
+# install sw
+
+npm install -g webshot-cli
+
+# cleanup packages
+
+dpkg -P build-essential
+
+# cleanup install
+
+apt-get -y autoremove
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+mkdir /app
+
+ln -s /usr/local/bin/node /usr/bin/node
